a7695e54749e028dbe35f551b92130751d434c12
dead-dreammer <152045804+dead-dreammer@users.noreply.github.com>
2024-05-08 17:55:56 +0000

    part2

--- chatbot/nltk_utils.py
+++ chatbot/nltk_utils.py
@@ -1,6 +1,34 @@
-import numpy as np
 import nltk
-# nltk.download('punkt')
+import numpy as np
 from nltk.stem.porter import PorterStemmer
+
 stemmer = PorterStemmer()
 
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+#not important, checks code works
+words = ["organize", "organizes", "organizing"]
+stemmed_words = [stem(w) for w in words]
+print(stemmed_words)
+
+
+def bag_of_words(tokenized_sentence, all_words):
+
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
+
+# this code below should return [0,1,0,1,0,0,0] , its just for check that it works
+#its not important, can remove if it works
+sentence = ["hello", "how", "are", "you"]
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bag = bag_of_words(sentence, words)
+print(bag)
